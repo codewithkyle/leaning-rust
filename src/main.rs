2c64f86b9fe1fe2c7601b781f78351ab1b3aa56b
Kyle Andrews <codingwithkyle@gmail.com>
2023-03-11 12:00:09 -0500

    Added Shapes mod

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,15 @@
+mod shapes;
+
+use crate::shapes::{rect::Rect, circle::Circle, area::Area};
+
 fn main(){
+    let rect = Rect::default();
+    let circle = Circle {
+        x: 0.0,
+        y: 0.0,
+        radius: 10.0,
+    };
+    println!("{}", circle.area());
+    println!("{}", rect);
+    println!("{}", rect.area());
 }
